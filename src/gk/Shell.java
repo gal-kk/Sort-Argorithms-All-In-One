c9378cd096104552fa70a8f6908e75533462697f
Åtrå <1311168sd@gmail.com>
2020-09-22 09:11:18 +0800

    Update Shell.java

--- src/gk/Shell.java
+++ src/gk/Shell.java
@@ -1,24 +1,42 @@
 package gk;
 
 public class Shell {
-    public int[] sort(int[] nums) {
-        int temp;
-        int gap = nums.length/2;
-        while (gap >=1){
-            for (int i = 0;i< gap; i++){
-                for (int j = gap + i; j< nums.length; j+=gap){
-                    for (int l= j; l>gap -1; l-=gap){
-                        if (nums[l] < nums[l-gap]){
-                            temp = nums[l];
-                            nums[l] = nums[l-gap];
-                            nums[l-gap] = temp;
-                        }
+    public int[] sort(int[] arr) {
+
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // 从第gap个元素，逐个对其所在的组进行直接插入排序
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (arr[j] < arr[j - gap]) {
+                    while (j - gap >= 0 && temp < arr[j - gap]) {
+                        //移动
+                        arr[j] = arr[j-gap];
+                        j -= gap;
                     }
+                    //当退出while后，就给temp找到插入的位置
+                    arr[j] = temp;
                 }
+
             }
-            gap /= 2;
         }
-        return nums;
+        return arr;
+//        int gap = nums.length/2;
+//        while (gap >=1){
+//            for (int i = gap; i<nums.length; i++){
+//                int j = i;
+//                int temp = nums[j];
+//                if (temp<nums[j-gap]){
+//                    while(j-gap>=0 && temp < nums[j-gap]){
+//                        nums[j] = nums[j-gap];
+//                        j-=gap;
+//                    }
+//                }
+//                nums[j] = temp;
+//            }
+//            gap /= 2;
+//        }
+//        return nums;
     }
 }
 
