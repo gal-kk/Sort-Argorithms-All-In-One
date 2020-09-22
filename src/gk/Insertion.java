b05e3493ce5b0f2b628169962eb81c3c37df28f5
Åtrå <1311168sd@gmail.com>
2020-09-22 09:11:02 +0800

    Update Insertion.java

--- src/gk/Insertion.java
+++ src/gk/Insertion.java
@@ -2,16 +2,27 @@ package gk;
 
 public class Insertion {
     public int[] sort(int[] nums){
-        int temp;
+
+//        for (int i = 1; i< nums.length; i++){
+//            for (int j = i; j>0; j--){
+//                if (nums[j] < nums[j-1]){
+//                    int temp = nums[j];
+//                    nums[j] = nums[j-1];
+//                    nums[j-1] = temp;
+//                }else
+//                    break;
+//            }
+//        }
+
         for (int i = 1; i< nums.length; i++){
-            for (int j = i; j>0; j--){
-                if (nums[j] < nums[j-1]){
-                    temp = nums[j];
+            int min = nums[i];
+            int j;
+            for (j = i; j>0 && min < nums[j-1]; j--){
                     nums[j] = nums[j-1];
-                    nums[j-1] = temp;
-                }
             }
+            nums[j] = min;
         }
+
         return nums;
     }
 }
